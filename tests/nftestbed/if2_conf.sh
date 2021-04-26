7286a9b1c0639c7d8b0bdca4c89c9c09f7094f55
vpasias <52623837+vpasias@users.noreply.github.com>
2021-04-26 16:54:50 +0300

    Update if2_conf.sh

--- tests/nftestbed/if2_conf.sh
+++ tests/nftestbed/if2_conf.sh
@@ -22,8 +22,8 @@ cd -
 python /home/vagrant/flexirouter/vpp/extras/vpp_config/scripts/dpdk-devbind.py -b e1000 00:01.0 00:02.0
 
 ip addr add $IP1/24 dev eth1
-ip link set dev eth1 mtu 1550
+ip link set dev eth1 mtu 1500
 ip link set dev eth1 up
 ip addr add $IP2/24 dev eth2
-ip link set dev eth2 mtu 1550
+ip link set dev eth2 mtu 1500
 ip link set dev eth2 up
