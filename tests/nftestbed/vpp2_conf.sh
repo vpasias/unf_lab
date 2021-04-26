2c1c4da323ce1b005dba0fb40b9e41af3e4e9351
vpasias <52623837+vpasias@users.noreply.github.com>
2021-04-26 16:55:03 +0300

    Update vpp2_conf.sh

--- tests/nftestbed/vpp2_conf.sh
+++ tests/nftestbed/vpp2_conf.sh
@@ -17,8 +17,8 @@ ip link set dev eth2 down
 $VPP_PATH/vppctl exec $SCRIPTS_PATH/init.cli
 
 ip addr add $IP1/24 dev vpp0
-ip link set dev vpp0 mtu 1550
+ip link set dev vpp0 mtu 1500
 ip link set dev vpp0 up
 ip addr add $IP2/24 dev vpp1
-ip link set dev vpp1 mtu 1550
+ip link set dev vpp1 mtu 1500
 ip link set dev vpp1 up
