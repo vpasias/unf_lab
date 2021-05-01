029fe043f1d3de6f02cde1085151c9cc773b4461
vpasias <52623837+vpasias@users.noreply.github.com>
2021-05-01 16:01:13 +0300

    Update if2_conf.sh

--- tests/nftestbed/if2_conf.sh
+++ tests/nftestbed/if2_conf.sh
@@ -11,10 +11,10 @@ IP2=$2
 /sbin/modprobe uio_pci_generic
 lspci | grep "Ethernet"
 
-sudo apt-get install gdb -y
+apt-get install gdb -y
 
-ip link set dev eth1 down
-ip link set dev eth2 down
+ip link set dev vpp0 down
+ip link set dev vpp1 down
 
 cd /home/vagrant/flexirouter/vpp/build-root/build-vpp_debug-native/vpp/bin/
 
@@ -24,9 +24,9 @@ cd -
 
 python /home/vagrant/flexirouter/vpp/extras/vpp_config/scripts/dpdk-devbind.py -b e1000 00:08.0 00:09.0
 
-ip addr add $IP1/24 dev eth1
-ip link set dev eth1 mtu 1500
-ip link set dev eth1 up
-ip addr add $IP2/24 dev eth2
-ip link set dev eth2 mtu 1500
-ip link set dev eth2 up
+ip addr add $IP1/24 dev vpp0
+ip link set dev vpp0 mtu 1500
+ip link set dev vpp0 up
+ip addr add $IP2/24 dev vpp1
+ip link set dev vpp1 mtu 1500
+ip link set dev vpp1 up
