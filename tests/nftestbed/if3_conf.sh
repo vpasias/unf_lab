dc4f34553419dd5388b1c2567b7c461d63912826
vpasias <52623837+vpasias@users.noreply.github.com>
2021-05-01 16:03:41 +0300

    Update if3_conf.sh

--- tests/nftestbed/if3_conf.sh
+++ tests/nftestbed/if3_conf.sh
@@ -10,25 +10,29 @@ IP2=$2
 IP2=$3
 
 /sbin/modprobe uio_pci_generic
+lspci | grep "Ethernet"
 
-ip link set dev eth1 down
-ip link set dev eth2 down
-ip link set dev eth3 down
+apt-get install gdb -y
 
-cd /home/vagrant/flexirouter/vpp
+ip link set dev vpp0 down
+ip link set dev vpp1 down
+ip link set dev vpp2 down
 
-gdb --args ./build-root/build-vpp_debug-native/vpp/bin/vpp -c /home/vagrant/flexirouter/scripts/startup.conf
+cd /home/vagrant/flexirouter/vpp/build-root/build-vpp_debug-native/vpp/bin/
+
+gdb --args vpp -c /home/vagrant/flexirouter/vpp/scripts/startup.conf
 
 cd -
 
-python /home/vagrant/flexirouter/vpp/extras/vpp_config/scripts/dpdk-devbind.py -b e1000 00:01.0 00:02.0 00:03.0
+python /home/vagrant/flexirouter/vpp/extras/vpp_config/scripts/dpdk-devbind.py -b e1000 00:08.0 00:09.0 00:12.0
 
-ip addr add $IP1/24 dev eth1
-ip link set dev eth1 mtu 1500
-ip link set dev eth1 up
-ip addr add $IP2/24 dev eth2
-ip link set dev eth2 mtu 1500
-ip link set dev eth2 up
-ip addr add $IP3/24 dev eth3
-ip link set dev eth3 mtu 1500
-ip link set dev eth3 up
+ip addr add $IP1/24 dev vpp0
+ip link set dev vpp0 mtu 1500
+ip link set dev vpp0 up
+ip addr add $IP2/24 dev vpp1
+ip link set dev vpp1 mtu 1500
+ip link set dev vpp1 up
+ip addr add $IP3/24 dev vpp2
+ip link set dev vpp2 mtu 1500
+ip link set dev vpp2 up
+
